ca01cdebc5bde01026f834eebcaa6ddc220463fc
Dempsey <dempsey.rhian.neal@gmail.com>
2021-02-19 18:39:58 +0000

    added new functions createPost and create for form challenge

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -3,14 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Owners;
+use App\Models\Owner;
 
 class OwnerController extends Controller
 {
-    public function index()
-    {
-        $ownerFromDB = Owners::all();
-        return view('welcome', ['owners' => $ownerFromDb]);
+   
+
+    public function show(Owner $owner)
+    {// shows the owners id
+        return view ('owners/show', ['singleOwner' => $owner]); 
     }
-};
-// Make sure you pass through the owners from the controller
+
+    public function create()
+    {
+        return view('owners/form'); //this loads the form.blade.php
+    }
+
+    public function index()
+    { // Make sure you pass through the owners from the controller
+        $ownerFromDb = Owner::all();
+        return view('welcome', ['bananas' => $ownerFromDb]);
+    } 
+
+
+
+    // accept the Request object
+    // this gives us access to the submitted data
+    public function createPost(Request $request)
+    {
+    // get all of the submitted data
+    $data = $request->all();
+
+    // create a new owner, passing in the submitted data
+    $owner = Owner::create($data);
+
+    // redirect the browser to the new owner
+    return redirect("/owners/{$owner->id}");
+    }
+}
+
