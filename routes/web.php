457dcf8abe64e69eafa0cde78c84f8405902a2e3
Dempsey <dempsey.rhian.neal@gmail.com>
2021-02-19 18:54:28 +0000

    added a route functionenclosing routes that link to url and ownercontroller class with create, createPost and show method

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,25 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OwnerController;
 
-Route::get('/', [OwnerController::class, 'index']);
+Route::get('/owners', [OwnerController::class, 'index']);
 
+/*
 Route::get('/', [Home::class, 'index']);
+Route::get('/owners/create', [OwnerController::class, 'index']);
+*/
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(['prefix' => 'owners'], function () {
+    // add *above* route with URL parameter
+    // otherwise 'create' will get included in that
+    Route::get('create', [OwnerController::class, 'create']);//owner/create
+    Route::post('create', [OwnerController::class, 'createPost']);
+    Route::get('{owner}', [OwnerController::class, 'show']);//owner/id  
+    //first 'create' is the url, second 'create' is part of ownercontroller.
 });
 
+/* http://192.168.10.10/owners to show in browser the owners
+http://192.168.10.10/owners/create to show in browser the form to create an owner */
 /*
 |--------------------------------------------------------------------------
 | Web Routes
