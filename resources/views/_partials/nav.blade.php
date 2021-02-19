dbe926f680e05af61577226943ebcb15fdad435a
Dempsey <dempsey.rhian.neal@gmail.com>
2021-02-19 18:50:04 +0000

    added link titled create owner which links to form being shown on browser

--- resources/views/_partials/nav.blade.php
+++ resources/views/_partials/nav.blade.php
@@ -2,4 +2,8 @@
     <a href="Home">Home</a>
     <a href="Blog">Blog</a>
     <a href="Contact">Contact</a>
+    <a href="/owners/create">Create Owner</a>
 </nav>
+
+ {{-- http://192.168.10.10/owners to show in browser the owners
+http://192.168.10.10/owners/create to show in browser the form to create an owner  --}}
